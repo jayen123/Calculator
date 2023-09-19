42beb906d4f7202fe69547b5103d001f489de281
Jayen Hasan <jayenhasan2003@gmail.com>
2023-09-19 10:42:36 +0600

    design ready

---
+++ dist/js/custom.js
@@ -0,0 +1,8 @@
+let btn = document.querySelectorAll('button');
+
+let result = document.querySelector('input');
+
+btn.addEventListener('click',function(){
+    console.log(btn);
+});
+
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./dist/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      container : {
+        center : true
+      }
+    },
   },
   plugins: [],
 }
